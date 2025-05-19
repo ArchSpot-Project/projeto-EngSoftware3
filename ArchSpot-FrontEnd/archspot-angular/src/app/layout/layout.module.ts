36d77428be82f5311adae22a635e3da6e89bc57e
erossecundus <erossecundus@gmail.com>
2025-05-19 00:15:51 -0300

    feat: Criacao menu navegacao sidebar; criacao templates de paginas

--- ArchSpot-FrontEnd/archspot-angular/src/app/layout/layout.module.ts
+++ ArchSpot-FrontEnd/archspot-angular/src/app/layout/layout.module.ts
@@ -4,13 +4,19 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './footer/footer.component';
 import { RouterLinkActive, RouterModule } from '@angular/router';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
+import { PublicPageLayoutComponent } from './public-page-layout/public-page-layout.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarMenuComponent,
+    PageLayoutComponent,
+    PublicPageLayoutComponent
   ],
   imports: [
     CommonModule,
@@ -19,7 +25,9 @@ import { RouterLinkActive, RouterModule } from '@angular/router';
   ],
   exports: [
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarMenuComponent,
+    PageLayoutComponent
   ]
 })
 export class LayoutModule { }
